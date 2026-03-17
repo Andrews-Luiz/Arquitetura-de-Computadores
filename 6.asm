cd12a6e68b48bbd6f4bdb0f22d2bba9f50dd55ac
Andrews Luiz <andrewsfilho2005@gmail.com>
2026-03-17 11:52:33 +0000

    Adicionado novo arquivo

---
+++ 6.asm
@@ -0,0 +1,38 @@
+# Faça um programa que leia um número entre 0 e 999 e imprima esse número com 3 algarismos. Exemplo, 23 gera uma saída 023 e 8 gera uma saída 008.
+
+.text
+main: 
+     li $2, 5 # aciona o serviço 5, que lê um número inteiro
+     syscall 
+     
+     move $8, $2 # pega o conteúdo do registrador 2 e move pro 8
+     
+     li $16, 100 
+     li $17, 10
+     li $18, 1
+     
+     div $8, $16 # pega 23 e divide por 100, gera quociente 0  e resto 23
+     mflo $11 # pega o 0 que está em lo e move pro registrador 4
+     mfhi $9 # pega o que está em hi e move pro registrador 9
+     
+     div $9 $17 # 3 resto, e 2 de quociente
+     mflo $12
+     mfhi $10
+     
+     div $10 $18
+     mflo $13
+     
+     move $4, $11
+     li $2, 1 # imprime o conteúdo do registrador 4
+     syscall
+     
+     move $4, $12
+     li $2, 1
+     syscall
+     
+     move $4, $13
+     li $2, 1
+     syscall
+     
+     
+     
