34c29fd6a0ce13798a252b132d1076b2f0dc9f87
Andrews Luiz <andrewsfilho2005@gmail.com>
2026-03-19 20:25:48 +0000

    arquivo alterado

--- Lista 1/10Maiscula.asm
+++ Lista 1/10Maiscula.asm
@@ -1,15 +1,19 @@
 .text
 main:
-     li $2, 12 # l� um caractere
+     li $2, 12 # l� um caractere, ao acionar o comando 12 de leitura de caractere
      syscall
      
      move $8, $2
      
-     li $16, 32
+     li $2, 11 # syscall pra imprimir o caractere lido
+     li $4, 10 # código Ascii, para /n, quebra de linha
+     syscall
      
-     sub $4, $8, $16
+     li $16, 32 # pega a constante 32 e coloca no registrador 16
      
-     li $2, 11
+     sub $4, $8, $16 # faz a subtração do caractere minusculo, cuja sua subtração por 32 será seu caractere maisculo e aloca a resposta no registrador 4
+     
+     li $2, 11 # imprime o conteudo do registrador 4
      syscall
      
      li $2, 10
